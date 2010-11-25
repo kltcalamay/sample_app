772107bb28a0caab338bce7bb90b7118851eca46
Kenneth <kltcalamay@gmail.com>
2010-11-25 16:39:01 +0800

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
+  match '/signup',  :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
